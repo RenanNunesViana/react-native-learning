1d6723a993095519b6dcaa998278fd4124daa4c9
RenanNunesViana <renan.devops.viana@gmail.com>
2024-11-26 21:04:19 -0300

    refatorando EdicaoCartaoScreen.js. Modificando FirebaseConfig.js para uso de variaveis de ambiente nas credenciais do bd.

--- StudyApp/src/config/FirebaseConfig.js
+++ StudyApp/src/config/FirebaseConfig.js
@@ -1,16 +1,25 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
+import {
+    FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID,
+} from '@env'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBpU61wVbl5UDcRUwVQ-afT0FAn9G0FLSk",
-    authDomain: "study-app-2024-2.firebaseapp.com",
-    projectId: "study-app-2024-2",
-    storageBucket: "study-app-2024-2.firebasestorage.app",
-    messagingSenderId: "949315200834",
-    appId: "1:949315200834:web:2b85c465326a76064a4adb",
-    measurementId: "G-4DPQEWEV8V"
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+    measurementId: FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
